03e870acc41a8ce7414942c0c5c4132753d7c62f
MatheusRCapanema <matheus.rocap@gmail.com>
2022-01-14 09:20:03 -0300

    Tentando fazer o edit

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -3,11 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\ValidacaoTicket;
 use App\Models\Ticket;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Input; 
+use Illuminate\Support\Facades\Input;
 
 class TicketController extends Controller
 {
@@ -23,31 +22,41 @@ class TicketController extends Controller
         return view('visual-action', compact('ticket') );
     }
 
+
     public function criarTickets()
     {
         return view('criar');
     }
 
+
+
+    public function armazenarTickets(Request $request)
+    {
+        $ticketData =  $request->all();
+        $ticketData['fk_Usu_Solicitante'] = $request->user()->id;
+        $ticketData['fk_Area_Atendente']= $request->user()->fk_Area;
+        $ticketData['fk_status'] = 4;
+
+        Ticket::create($ticketData);
+
+        return redirect()->route('ticket.list');
+    }
+
     public function editarTickets($id)
     {
-        $ticket = Ticket::findOrFail($id);   
-        return view('editar', compact('ticket'));
+        $ticket = Ticket::findOrFail($id);
+        return view('ticket.editar',['ticket'=>$ticket]);
+    }
+    public function update($id, Request $request){
+        $this->validate($request,[
+            'Assunto'=>'required',
+            'Problema'=>'required'
+        ]);
+        $ticketData=$request->all();
+        Ticket::findOrFail($id)->update($ticketData);
+        Session::flash('success','Ticket atualizado com sucesso');
+        return redirect()-route('ticket.list');
     }
 
-
-    public function armazenarTickets(ValidacaoTicket $request)
-    {
-           $ticketData =  $request->all();
-           $ticketData['fk_Usu_Solicitante'] = $request->user()->id;
-           $ticketData['fk_Area_Atendente']= $request->user()->fk_Area;
-           $ticketData['fk_status'] = 4;
-           
-           Ticket::create($ticketData);
-
-            return redirect()->route('ticket.list');  
-    }
-
-
-   
 
 }
--- resources/views/editar.blade.php
+++ resources/views/editar.blade.php
@@ -9,26 +9,45 @@
     <title>Editar Ticket</title>
 </head>
 <body>
-    @include('layouts._partials.topo')
-    <div class="w-full">
-        <div class="bg-gradient-to-b from-blue-800 to-blue-600 h-96"></div>
-        <div class="max-w-6xl mx-auto px-6 sm:px-6 lg:px-8 mb-12">
-            <div class="bg-white w-full shadow rounded p-8 sm:p-12 -mt-72" style="width: 1076px">
-                <p class="text-3xl font-bold leading-7 text-center">Visualizar Tickets</p>
-                <form action="{{route('ticket.editId')}}" method="GET">
-                    @csrf
-      
-                    @include('components.show-ticket')  
-                    
-                    <div class="flex items-center justify-center w-full">
-                        <button type="submit" name="" class="mt-9 font-semibold leading-none text-white py-4 px-10 bg-blue-700 rounded hover:bg-blue-600 focus:ring-2 focus:ring-offset-2 focus:ring-blue-700 focus:outline-none">
-                            Voltar
-                        </button>
+@include('layouts._partials.topo')
+<div class="w-full">
+    <div class="bg-gradient-to-b from-blue-800 to-blue-600 h-96"></div>
+    <div class="max-w-5xl mx-auto px-6 sm:px-6 lg:px-8 mb-12">
+        <div class="bg-white w-full shadow rounded p-8 sm:p-12 -mt-72">
+            <p class="text-3xl font-bold leading-7 text-center">Editar Ticket</p>
+
+            <form action="{{route('ticket.update')}}" method="post">
+                @csrf
+                <div class="md:flex items-center mt-12">
+                    <div class="w-full md:w-1/2 flex flex-col">
+                        <label class="font-semibold leading-none">Assunto</label>
+                        <input type="text" name="Assunto" id="Assunto" placeholder="Assunto" class="leading-none text-gray-900 p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200" value="{{$ticket.assunto}}" />
                     </div>
-                </form>
-            </div>
+                    <div class="w-full md:w-1/2 flex flex-col md:ml-6 md:mt-0 mt-4">
+                        <label class="font-semibold leading-none">Prioridade</label>
+                        <select name="fk_Prioridade" id= "fk_Prioridade" class="leading-none text-gray-900 p-3 focus:outline-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200">
+                            <option value="1">Baixa</option>
+                            <option value="2">Media</option>
+                            <option value="3">Alta</option>
+                            <option value="4">Urgente</option>
+                        </select>
+                    </div>
+                </div>
+                <div>
+                    <div class="w-full flex flex-col mt-8">
+                        <label class="font-semibold leading-none">Problema</label>
+                        <textarea name="Problema" id="Problema" cols="30" rows="4" placeholder="Conteúdo" type="text" class="h-40 text-base leading-none text-gray-900 p-3 focus:oultine-none focus:border-blue-700 mt-4 bg-gray-100 border rounded border-gray-200">{{$ticket->problema}}</textarea>
+                    </div>
+                </div>
+                <div class="flex items-center justify-center w-full">
+                    <button type="submit" name="" class="mt-9 font-semibold leading-none text-white py-4 px-10 bg-blue-700 rounded hover:bg-blue-600 focus:ring-2 focus:ring-offset-2 focus:ring-blue-700 focus:outline-none">
+                        Editar
+                    </button>
+                </div>
+            </form>
         </div>
     </div>
+</div>
 
 </body>
 </html>
